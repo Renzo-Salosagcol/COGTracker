ee0ff67cf2a046b5f21d17ea2471a315372ffe6a
renzoSalosagcol <renzosalosagcol@csu.fullerton.edu>
2026-05-06 13:41:16 -0700

    first commit

---
+++ .history/src/App_20260506133049.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-white font-bold underline">App</h1>
+    </>
+  )
+}
---
+++ .history/src/App_20260506133053.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-redfont-bold underline">App</h1>
+    </>
+  )
+}
